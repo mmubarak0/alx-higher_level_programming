3ddd96d9dfad1e62531de706bcdb23aca36b9dd6
Mohammed Mubarak <modymu9@gmail.com>
2023-08-16 23:06:50 +0200

    added last mandatory task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    return 1
+    roman_numbers = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+        }
+    subtractive_notation = False
+    result = 0
+    if type(roman_string) is str:
+        for idx, num in enumerate(roman_string):
+            if idx and\
+                    roman_numbers[num] > roman_numbers[roman_string[idx - 1]]:
+                subtractive_notation = True
+            if not subtractive_notation:
+                result += roman_numbers[num]
+            else:
+                result += roman_numbers[num]
+                result -= roman_numbers[roman_string[idx - 1]] * 2
+            subtractive_notation = False
+    return result
