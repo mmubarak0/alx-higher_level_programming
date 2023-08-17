0aa6e53645c8b7fe12d8f884e34f351c5795bef0
Mohammed Mubarak <modymu9@gmail.com>
2023-08-17 03:25:02 +0200

    fix task 100 case dividing by zero

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
     result = 0.0
     numerator = 0.0
     denominator = 0.0
-    if type(my_list) is list:
+    if type(my_list) is list and len(my_list) > 0:
         for x, y in my_list:
             numerator += x * y
             denominator += y
