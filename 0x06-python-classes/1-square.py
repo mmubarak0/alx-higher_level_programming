47d54e58ef2617c1829277f5fd2b6d20aaf4f8fa
Mohammed Mubarak <modymu9@gmail.com>
2023-08-29 19:09:21 +0200

    added task 0, 1, and 2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square."""
+
+
+class Square:
+    """empty square class."""
+
+    def __init__(self, size):
+        """Initialize the square instance.
+
+        Args:
+            size (int): square size.
+        """
+        self.__size = size
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square."""
+
+
+class Square:
+    """empty square class."""
+
+    def __init__(self, size=0):
+        """Initialize the square instance.
+
+        Args:
+            size (int): square size.
+        """
+        if type(size) is not int:
+            self.__size = 0
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            self.__size = 0
+            raise ValueError("size must be >= 0")
+        self.__size = size
