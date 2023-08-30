d10eb3c203fc4e485ad9b7844b03995ac18fb2ff
Mohammed Mubarak <modymu9@gmail.com>
2023-08-30 20:59:24 +0200

    added third advanced task

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square."""
+
+
+class Square:
+    """empty square class."""
+
+    def __init__(self, size=0):
+        """Initialize the square instance.
+
+        Args:
+            size (int): square size.
+        """
+        if type(size) is not int:
+            self.__size = 0
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            self.__size = 0
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Return the private variable size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the value of private variable size."""
+        if type(value) is not int:
+            self.__size = 0
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            self.__size = 0
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __eq__(self, square):
+        """Check equality."""
+        return self.area() == square.area()
+
+    def __ne__(self, square):
+        """Check inequality."""
+        return self.area() != square.area()
+
+    def __lt__(self, square):
+        """Less than."""
+        return self.area() < square.area()
+
+    def __le__(self, square):
+        """Less than or equal to."""
+        return self.area() <= square.area()
+
+    def __gt__(self, square):
+        """Greater than."""
+        return self.area() > square.area()
+
+    def __ge__(self, square):
+        """Greater than or equal to."""
+        return self.area() >= square.area()
