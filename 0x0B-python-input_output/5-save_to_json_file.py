62c91585719bd1ed1c548381cccaf9845281d7a9
Mohammed Mubarak <modymu9@gmail.com>
2023-09-13 05:05:33 +0200

    fixed task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,3 @@ def save_to_json_file(my_obj, filename):
     """Save obj to file.json."""
     with open(filename, "w", encoding="utf-8") as f:
         f.write(json.dumps(my_obj))
-        f.write('\n')
