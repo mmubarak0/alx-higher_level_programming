2fec2f1d2f15b2d9504fa1e662f26871a3119b87
Mohammed Mubarak <modymu9@gmail.com>
2023-09-19 14:32:38 +0200

    added test for empty save to file method

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -85,6 +85,10 @@ class TestBaseClass(unittest.TestCase):
         with open("Rectangle.json", "r") as file:
             self.assertEqual([], json.loads(file.read()))
 
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            self.assertEqual([], json.loads(file.read()))
+
     def test_7(self):
         """From_json_string method."""
         list_input = [
