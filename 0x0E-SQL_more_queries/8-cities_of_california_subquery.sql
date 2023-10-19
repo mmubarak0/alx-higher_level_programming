ed037562edf7814b609ac3db05b0ab4e10cb4456
Mohammed Mubarak <modymu9@gmail.com>
2023-10-19 23:13:01 +0200

    fixed error in task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT * FROM cities AS c
+SELECT id, name FROM cities AS c
 WHERE state_id = (SELECT DISTINCT id FROM states WHERE name = "California")
 ORDER BY c.id;
