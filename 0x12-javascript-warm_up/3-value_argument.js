73b330ae954113c05b226c61737bd6336a961288
Mohammed Mubarak <modymu9@gmail.com>
2023-11-14 02:39:49 +0200

    fix task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,8 @@
 const err = 'No argument';
 const argv = process.argv;
 
-if (argv[2]) {
-  console.log(...argv[2]);
-} else {
+if (!argv[2]) {
   console.log(err);
+} else {
+  console.log(argv[2]);
 }
