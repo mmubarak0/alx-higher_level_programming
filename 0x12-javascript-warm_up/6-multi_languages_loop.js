7fcf974b67037799dfcf9313dc71d0d04efd9f61
Mohammed Mubarak <modymu9@gmail.com>
2023-11-14 02:51:09 +0200

    added task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
+const arr = [a, b, c];
+
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
