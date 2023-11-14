147cff4fcfe357ffa710dc4e078f2fa4eef15a60
Mohammed Mubarak <modymu9@gmail.com>
2023-11-14 03:18:35 +0200

    Added task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+console.log(factorial(Number(argv[2])));
+
+function factorial (a) {
+  if (!a)
+    return (1);
+  return a * factorial(a - 1);
+}
+
