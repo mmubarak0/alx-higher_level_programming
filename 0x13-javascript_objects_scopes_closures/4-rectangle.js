44a6a1b86dfc9d6cbb596a2fdf9f7656402857e6
Mohammed Mubarak <modymu9@gmail.com>
2023-11-15 05:34:57 +0200

    fix task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -21,8 +21,7 @@ module.exports = class Rectangle {
   }
 
   double () {
-    const temp = this.width;
-    this.width = this.height * 2;
-    this.height = temp * 2;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
