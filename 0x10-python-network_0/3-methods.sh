4fd5611e69a815870e310ab6b9bbbd93847c4b5a
Mohammed Mubarak <modymu9@gmail.com>
2023-11-30 19:15:43 +0200

    using grep instead of selecting line by tail head

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all HTTP methods the server will accept.
-curl -sI $1 | tail -n2 | head -n1 | cut -d" " -f1 --complement;
+curl -sI $1 | grep Allow | cut -d" " -f1 --complement;
