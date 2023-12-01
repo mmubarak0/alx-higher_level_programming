c973d2bde81424f5e4db8fe9b77a62bfac603fa9
Mohammed Mubarak <modymu9@gmail.com>
2023-12-02 00:16:35 +0200

    code must not be executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """sends a request to the URL and displays the value of the X-Request-Id."""
 
-import sys
-import urllib.request as request
-import urllib.parse as parse
+if __name__ == "__main__":
+    import sys
+    import urllib.request as request
+    import urllib.parse as parse
 
-values = {'name': 'Michael Foord'}
-req = request.Request(sys.argv[1], parse.urlencode(values).encode('ascii'))
-with request.urlopen(sys.argv[1]) as response:
-    content = response.getheader("X-Request-Id")
-    print(content)
+    values = {'name': 'Michael Foord'}
+    req = request.Request(sys.argv[1], parse.urlencode(values).encode('ascii'))
+    with request.urlopen(sys.argv[1]) as response:
+        content = response.getheader("X-Request-Id")
+        print(content)
