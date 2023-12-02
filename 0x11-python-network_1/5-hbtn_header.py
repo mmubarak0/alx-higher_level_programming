9c1c657b7ed7c5f1c7e4dfc185a5fcc3a5a1e2eb
Mohammed Mubarak <modymu9@gmail.com>
2023-12-02 02:38:36 +0200

    Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Sends a request to the URL and displays value of X-Request-Id."""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    r = requests.post(sys.argv[1], data=payload)
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
