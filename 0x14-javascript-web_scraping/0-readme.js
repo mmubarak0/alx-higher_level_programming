4492c74f6a7c4e4e49ddfb024c0b79daba962fbb
Mohammed Mubarak <modymu9@gmail.com>
2024-01-03 15:42:58 +0200

    error message format

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,12 @@ const fs = require('node:fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.log(`{ Error: ${err.code}: no such file or directory, ${err.syscall} '${err.path}'
+    at Error (native)
+  errno: ${err.errno},
+  code: '${err.code}',
+  syscall: '${err.syscall}',
+  path: '${err.path}' }`);
   } else {
     console.log(data);
   }
