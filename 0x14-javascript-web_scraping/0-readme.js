86251472ff3c32ad2c21b26a92c0f24c9f9f83c3
Mohammed Mubarak <modymu9@gmail.com>
2024-01-03 15:47:19 +0200

    just reverting back nothingis working

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,12 +4,7 @@ const fs = require('node:fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.log(`{ Error: ${err.code}: no such file or directory, ${err.syscall} '${err.path}'
-    at Error (native)
-  errno: ${err.errno},
-  code: '${err.code}',
-  syscall: '${err.syscall}',
-  path: '${err.path}' }`);
+    console.log(err);
   } else {
     console.log(data);
   }
