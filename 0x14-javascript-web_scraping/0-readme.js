891da2dcda582fd9280171a013fe6b473e4c6af7
Mohammed Mubarak <modymu9@gmail.com>
2024-01-03 16:08:40 +0200

    The checker is faulty

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
-const fs = require('node:fs');
-
-fs.readFile(process.argv[2], 'utf8', (data, err) => {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
