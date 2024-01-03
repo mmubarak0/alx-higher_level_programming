187a2e3211e890a48cb2c58fb60c6bb8db04c58f
Mohammed Mubarak <modymu9@gmail.com>
2024-01-03 19:57:13 +0200

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const content = process.argv[3];
+fs.writeFile(process.argv[2], content, 'utf8', function (err) {
+  if (err) {
+    console.error(err);
+  }
+});
