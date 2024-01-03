31b54eb019211c16f0dbc3770e327e15b3598d45
Mohammed Mubarak <modymu9@gmail.com>
2024-01-03 14:31:38 +0200

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('node:fs');
+
+
+fs.readFile(process.argv[2], 'utf8', (data, err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
